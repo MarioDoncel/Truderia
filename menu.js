df0d70a83b4f107eb16d57cee1b7be7a5b4b6adb
Mario Andres Doncel Neto <88mario.doncel@gmail.com>
2021-06-11 22:07:14 -0300

    Mudanca de formato e inicio de automação do HTML

---
+++ menu.js
@@ -0,0 +1,136 @@
+
+// Cardapio
+const Trudels = [
+    {
+        flavor:"Tradicional",
+        description:"Açúcar e canela.",
+        image:, 
+        price: 8
+    },
+    {
+        flavor:"DOCE DE LEITE",
+        description:"Recheado com um delicioso de doce de leite",
+        image:, 
+        price:10
+    },
+    {
+        flavor:"CHOCOLATE MEIO AMARGO",
+        description:"Recheado com o melhor meio amargo 70%!",
+        image:, 
+        price:11
+    },
+    {
+        flavor:"CHOCOLATE BRANCO",
+        description:"Recheado com chocolate branco de dar agua na boca.",
+        image:, 
+        price:12
+    },
+    {
+        flavor:"NUTELLA",
+        description:"Recheado com Nutella, o creme de avelã",
+        image:, 
+        price:12
+    },
+    {
+        flavor:"BRIGADEIRO",
+        description:"Recheado com brigadeiro feito com os melhores ingredientes.",
+        image:, 
+        price:12
+    },{
+        flavor:"KIT KAT CREMOSO",
+        description:"Recheado com um creme de KIT KAT que dispensa comentários.",
+        image:, 
+        price:12
+    },{
+        flavor:"OVOMALTINE",
+        description:"Recheado com creme do polêmico, crocante e irresistível OVOMALTINE.",
+        image:, 
+        price:13
+    },
+]
+
+const additionals = [
+    {
+        name:"MORANGO",
+        description:"(5 un)",
+        image:, 
+        price: 4
+    },  
+    {
+        name:"AMENDOIM",
+        description:"(5 un)",
+        image:, 
+        price:2
+    },  
+    {
+        name:"NOZES",
+        description:"",
+        image:, 
+        price: 2
+    },  
+    {
+        name:"GRANULADO",
+        description:"",
+        image:, 
+        price: 2
+    },  
+    {
+        name:"COCO RALADO",
+        description:"",
+        image:, 
+        price: 1
+    },  
+    {
+        name:"KIT KAT",
+        description:"",
+        image:,
+        price: 2
+    },  
+    {
+        name:"SONHO DE VALSA",
+        description:"",
+        image:,
+        price: 2
+    },  
+    {
+        name:"DOBRO DE RECHEIO",
+        description:"",
+        image:,
+        price: 4
+    },  
+
+]
+
+const iceCreams = [
+    {
+        flavor:"CREME",
+        image:,
+        price: 4
+    },
+    {
+        flavor:"NINHO TRUFADO",
+        image:,
+        price: 4
+    },
+    {
+        flavor:"IOGURTE COM FRUTAS VERMELHAS",
+        image:,
+        price: 4
+    },
+    {
+        flavor:"CAFÉ",
+        image:,
+        price: 4
+    },
+    {
+        flavor:"BEM CASADO",
+        image:,
+        price: 4
+    },
+    {
+        flavor:"SENSAÇÃO ",
+        image:,
+        price: 4
+    }
+]
+    
